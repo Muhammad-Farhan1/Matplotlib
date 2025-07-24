5a5d2ea30e97142b3fa8beb727d5a68dbe6dfad6
Muhammad Ahsan <180093008+Muhammad-Farhan1@users.noreply.github.com>
2025-07-24 08:25:38 +0500

    feat : Matplotlib graph

---
+++ matlplotlib.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.array([1,6])
+y = np.array([0,250])
+
+plt.plot(x,y)
+plt.title("Physics Diagram")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.show()
